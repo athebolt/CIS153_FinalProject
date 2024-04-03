bbe62361d9a8b269539888485dfac7b40d8fc4b5
SuperFire92 <nickdyk21@gmail.com>
2024-04-03 15:48:16 -0400

    Board And Cell Classes
    Added a Board and Cell class.
    The Cell class holds a value and x and y position.
    The Board class holds an array of 7x6 of cells, and can currently make moves. There is an unfinished function to fill the form with data from the board, but we need to set up the form to get that working.
    Also added a board into the Form1.cs, anyone feel free to get rid of that if we want it on a different form. Most of my changes on there are just for showing what I'm thinking with the game.

---
+++ ConnectFour_Group3/Cell.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFour_Group3
+{
+    public class Cell
+    {
+        private int xPos;
+        private int yPos;
+        private char val;
+
+        public Cell()
+        {
+
+        }
+
+        public Cell(int xPos, int yPos, char val)
+        {
+            this.xPos = xPos;
+            this.yPos = yPos;
+            this.val = val;
+        }
+
+        public bool isEmpty()
+        {
+            if (val == ' ')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int getXPos()
+        {
+            return xPos;
+        }
+        public int getYPos()
+        {
+            return yPos;
+        }
+        public char getVal()
+        {
+            return val;
+        }
+
+        public void setXPos(int xPos)
+        {
+            this.xPos = xPos;
+        }
+        public void setYPos(int yPos)
+        {
+            this.yPos = yPos;
+        }
+        public void setVal(char val)
+        {
+            this.val = val;
+        }
+    }
+}
--- ConnectFour_Group3/Form1.cs
+++ ConnectFour_Group3/Form1.cs
@@ -12,9 +12,26 @@ namespace ConnectFour_Group3
 {
     public partial class Form1 : Form
     {
+        public Board connectFourBoard = new Board();
+        //This is an array of all of the buttons (or whatever object we use to display the board, we can change this)
+        public Button[,] buttons;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void fillButtonArray()
+        {
+            //Fill the array manually with the name of every cell on the form, this will allow us to use a function on Board.cs to update the board on the form.
+            //I think Nathan said he was gonna work on this, so if u have questions about this Nathan, lmk. -Nick
+            buttons = new Button[,] {   { /*buttonA1, buttonB1, buttonC1, etc. */ },
+                                        { /*buttonA2, buttonB2, buttonC2, etc. */ },
+                                        { },
+                                        { },
+                                        { },
+                                        { },
+                                        { } };
         }
     }
 }
