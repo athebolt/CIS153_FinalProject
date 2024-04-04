60c9a8748ced7cf6a0ab9e3e5d5b4cc8898f01c9
pankeDev <ngessner04@gmail.com>
2024-04-04 14:29:34 -0400

    Create GameStats.cs
    added outline for stats

---
+++ ConnectFour_Group3/ConnectFour_Group3/GameStats.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectFour_Group3
+{
+    // plan on adding more soon
+    internal class GameStats
+    {
+        public int aiWins;
+        public int playerWins;
+        public int ties;
+        public double winPercentage;
+
+        public void saveStats()
+        {
+            // logic 
+        }
+        public void loadStats()
+        {
+            // logic
+        }
+        public void updateStats()
+        {
+            // logic
+        }        
+        public double calcWinPercentage(int wins)
+        {
+            int totalGames = aiWins + playerWins + ties;
+
+            if (totalGames > 0) 
+            {
+                winPercentage = (double)wins / totalGames * 100;
+            }
+            else
+            {
+                winPercentage = 0;
+            }
+
+            return winPercentage;
+        }
+    }
+}
