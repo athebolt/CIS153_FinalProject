4adf1dc37546bf367ef9a6588e7c27d7f71cc532
Tristan Mann <tb223one@gmail.com>
2024-05-02 19:30:51 -0400

    changed font of player display

--- ConnectFour_Group3/ConnectFour_Group3/Form1.Designer.cs
+++ ConnectFour_Group3/ConnectFour_Group3/Form1.Designer.cs
@@ -127,10 +127,10 @@
             // lblTurnDisp
             // 
             this.lblTurnDisp.AutoSize = true;
-            this.lblTurnDisp.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurnDisp.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTurnDisp.Location = new System.Drawing.Point(344, 24);
             this.lblTurnDisp.Name = "lblTurnDisp";
-            this.lblTurnDisp.Size = new System.Drawing.Size(195, 31);
+            this.lblTurnDisp.Size = new System.Drawing.Size(218, 36);
             this.lblTurnDisp.TabIndex = 54;
             this.lblTurnDisp.Text = "Player 1\'s Turn";
             // 
