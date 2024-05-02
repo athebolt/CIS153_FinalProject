ce6e56195f6d58eabac080dce10378adc30bf383
Tristan Mann <tb223one@gmail.com>
2024-05-02 14:59:10 -0400

    Updated title page

--- ConnectFour_Group3/ConnectFour_Group3/TitlePage.Designer.cs
+++ ConnectFour_Group3/ConnectFour_Group3/TitlePage.Designer.cs
@@ -31,11 +31,12 @@
             this.btnPlayGame = new System.Windows.Forms.Button();
             this.btnViewStats = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.lbltitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnPlayGame
             // 
-            this.btnPlayGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayGame.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPlayGame.Location = new System.Drawing.Point(275, 151);
             this.btnPlayGame.Name = "btnPlayGame";
             this.btnPlayGame.Size = new System.Drawing.Size(248, 65);
@@ -46,7 +47,7 @@
             // 
             // btnViewStats
             // 
-            this.btnViewStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewStats.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnViewStats.Location = new System.Drawing.Point(275, 222);
             this.btnViewStats.Name = "btnViewStats";
             this.btnViewStats.Size = new System.Drawing.Size(248, 65);
@@ -57,7 +58,7 @@
             // 
             // btnExit
             // 
-            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.Location = new System.Drawing.Point(275, 293);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(248, 65);
@@ -66,17 +67,29 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // lbltitle
+            // 
+            this.lbltitle.AutoSize = true;
+            this.lbltitle.Font = new System.Drawing.Font("Microsoft YaHei UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitle.Location = new System.Drawing.Point(225, 74);
+            this.lbltitle.Name = "lbltitle";
+            this.lbltitle.Size = new System.Drawing.Size(350, 64);
+            this.lbltitle.TabIndex = 3;
+            this.lbltitle.Text = "Connect Four";
+            // 
             // TitlePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbltitle);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnViewStats);
             this.Controls.Add(this.btnPlayGame);
             this.Name = "TitlePage";
             this.Text = "TitlePage";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +98,6 @@
         private System.Windows.Forms.Button btnPlayGame;
         private System.Windows.Forms.Button btnViewStats;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lbltitle;
     }
 }
